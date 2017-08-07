3f63a861bb5a6ad3bea2723f26568aeddcd90ab9
Roxana Robotin <rrobotin@mozilla.com>
2017-08-07 13:44:47 +0300

    updated output path

--- runTest.js
+++ runTest.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 Testing().then((data) => {
     var res = JSON.parse(JSON.stringify(data));
     console.log("testResult = " + JSON.stringify(res));
-    fs.writeFile('output.txt', JSON.stringify(res), (err) => {
+    fs.writeFile('roxana/output.txt', JSON.stringify(res), (err) => {
         if (err) throw err;
     console.log('The file has been saved!');
     });
